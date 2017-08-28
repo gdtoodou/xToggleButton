1bdc4b73b6b93402f0455a9951ddebd9e044c963
gdtoodou <butterknife@163.com>
2017-08-28 14:59:27 +0800

    first conmit

---
+++ app/src/main/java/cn/medicalpension/togglebuttondemo/MainActivity.java
@@ -0,0 +1,28 @@
+package cn.medicalpension.togglebuttondemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton tb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tb = (ToggleButton) findViewById(R.id.tb_select);
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(MainActivity.this,"选中",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"未选中",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
